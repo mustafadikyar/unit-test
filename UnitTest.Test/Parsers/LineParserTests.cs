9accf061de4d660ca2228cff5c804f5d20dc59de
Mustafa Dikyar <mustafa.dikyar@setur.com.tr>
2023-06-06 02:21:36 +0300

    First unit test was added.

---
+++ UnitTest.Test/Parsers/LineParserTests.cs
@@ -0,0 +1,20 @@
+﻿namespace UnitTest.Test.Parsers;
+
+public class LineParserTests
+{
+    [Fact]
+    public void ShouldParseValidLine()
+    {
+        //Arrange
+        List<string> lines = new() { "Cappuccino; 10/27/2022 8:22:01 AM" };
+
+        //Act
+        var dataItems = LineParsers.Parse(lines);
+
+        //Assert
+        Assert.NotNull(dataItems);
+        Assert.Single(dataItems);
+        Assert.Equal("Cappuccino", dataItems[0].CoffeeType);
+        Assert.Equal(new DateTime(2022, 10, 27, 8, 22, 01), dataItems[0].CreatedAt);
+    }
+}
--- UnitTest/Parsers/LineParsers.cs
+++ UnitTest/Parsers/LineParsers.cs
@@ -9,17 +9,14 @@ public class LineParsers
         foreach (var line in lines)
         {
             var dataItem = Parse(line);
-
             dataItems.Add(dataItem);
         }
-
         return dataItems.ToArray();
     }
 
     private static DataItem Parse(string line)
     {
         var lineItems = line.Split(';');
-
         return new DataItem(lineItems[0], DateTime.Parse(lineItems[1], CultureInfo.InvariantCulture));
     }
 }
